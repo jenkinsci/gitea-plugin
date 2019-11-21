2ff12182ffae18d6dd9cdab5a70b7f2ec186d50b
7FM <gitea@fake.local>
2019-11-21 11:47:16 +0100

    use subclass of DefaultGiteaCoonnection for testing

--- src/test/java/org/jenkinsci/plugin/gitea/client/impl/DefaultGiteaConnectionTest.java
+++ src/test/java/org/jenkinsci/plugin/gitea/client/impl/DefaultGiteaConnectionTest.java
@@ -1,8 +1,6 @@
 package org.jenkinsci.plugin.gitea.client.impl;
 
-import com.damnhandy.uri.template.UriTemplate;
 import org.jenkinsci.plugin.gitea.client.api.GiteaAuthNone;
-import org.jenkinsci.plugin.gitea.client.api.GiteaHttpStatusException;
 import org.jenkinsci.plugin.gitea.client.api.GiteaIssue;
 import org.jenkinsci.plugin.gitea.client.api.GiteaIssueState;
 import org.jenkinsci.plugin.gitea.client.api.GiteaOwner;
@@ -10,10 +8,6 @@ import org.jenkinsci.plugin.gitea.client.api.GiteaPullRequest;
 import org.jenkinsci.plugin.gitea.client.api.GiteaRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -21,42 +15,17 @@ import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
-import static org.powermock.api.support.membermodification.MemberMatcher.method;
 
 /**
  * Test cases for fetching pull requests or issues of a repository where those options are disabled
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(DefaultGiteaConnection.class)
 public class DefaultGiteaConnectionTest {
 
-    private DefaultGiteaConnection mockedDefaultGiteaConnection = new DefaultGiteaConnection("", new GiteaAuthNone());
+    private DefaultGiteaConnection giteaConnection = new GiteaConnection_DisabledPR_Issues("", new GiteaAuthNone());
     private GiteaRepository giteaRepository;
 
     @Before
-    public void setupMock() throws Exception {
-        // This is needed because openConnection is a static method
-        mockStatic(DefaultGiteaConnection.class);
-
-        /*
-         Set matcher for the argument of openConnection which decides when mocking takes place
-         We can't use this inside the when(...).withArguments() clause because it returns null causing a exception
-         (the method gets called once for setup)
-         Therefore we set the matcher outside to inform the mockito framework
-         and pass some valid arguments to the method to be mocked
-        */
-        ArgumentMatchers.notNull();
-
-        // Create mock
-        when(mockedDefaultGiteaConnection, method(DefaultGiteaConnection.class, "openConnection", UriTemplate.class))
-                // Pass valid argument needed to setup mock (the original method gets called once)
-                .withArguments(UriTemplate.buildFromTemplate("http://127.0.0.1").build())
-                // If this method gets called again it will throw a GiteaHttpStatusException
-                // simulating the response of the API if issues or PRs are disabled
-                .thenThrow(new GiteaHttpStatusException(404, "TEST CASE FAILED!"));
-
+    public void reset() {
         giteaRepository = new GiteaRepository(
                 new GiteaOwner("", "", "", ""),
                 null, "", "", "",
@@ -69,68 +38,68 @@ public class DefaultGiteaConnectionTest {
 
     @Test
     public void test_fetchPullRequests_with_disabled_prs__given_username_repoName_state() throws Exception {
-        List<GiteaPullRequest> pr = mockedDefaultGiteaConnection.fetchPullRequests("", "", Collections.singleton(GiteaIssueState.OPEN));
+        List<GiteaPullRequest> pr = giteaConnection.fetchPullRequests("", "", Collections.singleton(GiteaIssueState.OPEN));
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
-        pr = mockedDefaultGiteaConnection.fetchPullRequests("", "", Collections.singleton(GiteaIssueState.CLOSED));
+        pr = giteaConnection.fetchPullRequests("", "", Collections.singleton(GiteaIssueState.CLOSED));
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
     }
 
     @Test
     public void test_fetchPullRequests_with_disabled_prs__given_username_repoName() throws IOException, InterruptedException {
-        List<GiteaPullRequest> pr = mockedDefaultGiteaConnection.fetchPullRequests("", "");
+        List<GiteaPullRequest> pr = giteaConnection.fetchPullRequests("", "");
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
     }
 
     @Test
     public void test_fetchPullRequests_with_disabled_prs__given_repo() throws IOException, InterruptedException {
-        List<GiteaPullRequest> pr = mockedDefaultGiteaConnection.fetchPullRequests(giteaRepository);
+        List<GiteaPullRequest> pr = giteaConnection.fetchPullRequests(giteaRepository);
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
     }
 
     @Test
     public void test_fetchPullRequests_with_disabled_prs__given_repo_state() throws IOException, InterruptedException {
-        List<GiteaPullRequest> pr = mockedDefaultGiteaConnection.fetchPullRequests(giteaRepository, Collections.singleton(GiteaIssueState.OPEN));
+        List<GiteaPullRequest> pr = giteaConnection.fetchPullRequests(giteaRepository, Collections.singleton(GiteaIssueState.OPEN));
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
-        pr = mockedDefaultGiteaConnection.fetchPullRequests(giteaRepository, Collections.singleton(GiteaIssueState.CLOSED));
+        pr = giteaConnection.fetchPullRequests(giteaRepository, Collections.singleton(GiteaIssueState.CLOSED));
         assertNotNull(pr);
         assertTrue(pr.isEmpty());
     }
 
     @Test
     public void test_fetchIssues_with_disabled_issues__given_username_repoName_state() throws IOException, InterruptedException {
-        List<GiteaIssue> issues = mockedDefaultGiteaConnection.fetchIssues("", "", Collections.singleton(GiteaIssueState.OPEN));
+        List<GiteaIssue> issues = giteaConnection.fetchIssues("", "", Collections.singleton(GiteaIssueState.OPEN));
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
-        issues = mockedDefaultGiteaConnection.fetchIssues("", "", Collections.singleton(GiteaIssueState.CLOSED));
+        issues = giteaConnection.fetchIssues("", "", Collections.singleton(GiteaIssueState.CLOSED));
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
     }
 
     @Test
     public void test_fetchIssues_with_disabled_issues__given_repo_state() throws IOException, InterruptedException {
-        List<GiteaIssue> issues = mockedDefaultGiteaConnection.fetchIssues(giteaRepository, Collections.singleton(GiteaIssueState.OPEN));
+        List<GiteaIssue> issues = giteaConnection.fetchIssues(giteaRepository, Collections.singleton(GiteaIssueState.OPEN));
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
-        issues = mockedDefaultGiteaConnection.fetchIssues(giteaRepository, Collections.singleton(GiteaIssueState.CLOSED));
+        issues = giteaConnection.fetchIssues(giteaRepository, Collections.singleton(GiteaIssueState.CLOSED));
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
     }
 
     @Test
     public void test_fetchIssues_with_disabled_issues__given_repo() throws IOException, InterruptedException {
-        List<GiteaIssue> issues = mockedDefaultGiteaConnection.fetchIssues(giteaRepository);
+        List<GiteaIssue> issues = giteaConnection.fetchIssues(giteaRepository);
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
     }
 
     @Test
     public void test_fetchIssues_with_disabled_issues__given_username_repoName() throws IOException, InterruptedException {
-        List<GiteaIssue> issues = mockedDefaultGiteaConnection.fetchIssues("", "");
+        List<GiteaIssue> issues = giteaConnection.fetchIssues("", "");
         assertNotNull(issues);
         assertTrue(issues.isEmpty());
     }
