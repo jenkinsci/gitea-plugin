a6fd562ec5ca06cbcf1e95de94fe19165ae0fd6a
Mark Waite <mark.earl.waite@gmail.com>
2025-04-24 21:32:34 -0600

    Simplify implementation with single binding
    Testing done:
    
    Confirmed that the Pipeline syntax snippet generator shows the correct syntax, of the form:
    
    withCredentials([giteaPersonalAccessToken(credentialsId: 'my-gitea-token', variable: 'MY_VAR')]) {
        // some block
    }
    
    Confirmed that the localized form of the variable property and the
    credentials property are displayed as expected.
    
    Confirmed that my sample Pipeline worked:
    
    pipeline {
        agent {
            label 'alpine'
        }
        stages {
            stage('Checkout') {
                steps {
                    withCredentials([giteaPersonalAccessToken(credentialsId: 'gitea-token', variable: 'ABC')]) {
                      sh 'git clone http://$ABC@gitea-server.markwaite.net:3000/mwaite/bin.git'
                    }
                }
            }
        }
    }

--- src/main/java/org/jenkinsci/plugin/gitea/credentials/PersonalAccessTokenBinding.java
+++ src/main/java/org/jenkinsci/plugin/gitea/credentials/PersonalAccessTokenBinding.java
@@ -7,26 +7,17 @@ import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.util.Secret;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.credentialsbinding.Binding;
 import org.jenkinsci.plugins.credentialsbinding.BindingDescriptor;
-import org.jenkinsci.plugins.credentialsbinding.MultiBinding;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-public class PersonalAccessTokenBinding extends MultiBinding<PersonalAccessToken> {
-
-    // Environment variable name to be used in the binding
-    private final String variable;
+public class PersonalAccessTokenBinding extends Binding<PersonalAccessToken> {
 
     @DataBoundConstructor
     public PersonalAccessTokenBinding(String credentialsId, String variable) {
-        super(credentialsId);
-        this.variable = variable;
+        super(variable, credentialsId);
     }
 
     @Override
@@ -35,24 +26,13 @@ public class PersonalAccessTokenBinding extends MultiBinding<PersonalAccessToken
     }
 
     @Override
-    public Set<String> variables() {
-        // Return a set containing the environment variable name
-        return Collections.singleton(variable);
-    }
-
-    @Override
-    public MultiEnvironment bind(
-            @NonNull Run<?, ?> build,
-            @Nullable FilePath workspace,
-            @Nullable Launcher launcher,
-            @NonNull TaskListener listener)
-            throws IOException, InterruptedException {
-        // Retrieve the PersonalAccessToken credentials
-        PersonalAccessToken credentials = getCredentials(build);
-
-        Map<String, String> values = new LinkedHashMap<>();
-        values.put(variable, Secret.toString(credentials.getToken()));
-        return new MultiEnvironment(values);
+    public Binding.SingleEnvironment bindSingle(@NonNull Run<?,?> build,
+                                                @Nullable FilePath workspace,
+                                                @Nullable Launcher launcher,
+                                                @NonNull TaskListener listener)
+        throws IOException,InterruptedException {
+        PersonalAccessToken credential = getCredentials(build);
+        return new SingleEnvironment(credential.getToken().getPlainText());
     }
 
     @Symbol("giteaPersonalAccessToken") // Symbol annotation for use in Jenkins UI
