33b0b22b481e5bd880014ab5fa129cbadb33f455
Mark Waite <mark.earl.waite@gmail.com>
2025-04-25 03:53:40 -0600

    Simplify test script
    No need to declare the package of the script.  Use the default.
    
    No need to declare the full path to the PersonalAccessTokenBinding class,
    let Jenkins find it by searching.
    
    Add a newline to the end of the file.

--- src/test/resources/org/jenkinsci/plugin/gitea/credentials/pipeline/withCredentials-pipeline.groovy
+++ src/test/resources/org/jenkinsci/plugin/gitea/credentials/pipeline/withCredentials-pipeline.groovy
@@ -1,16 +1,14 @@
-package org.jenkinsci.plugin.gitea.credentials.pipeline
-
 node {
     withCredentials([[
-        $class: 'org.jenkinsci.plugin.gitea.credentials.PersonalAccessTokenBinding',
-        credentialsId: "personalAccessTokenId",
-        variable: "API_TOKEN1"
+        $class: 'PersonalAccessTokenBinding',
+        credentialsId: 'personalAccessTokenId',
+        variable: 'API_TOKEN1'
     ]]) {
         println "Token1 is ${API_TOKEN1.substring(1)}"
     }
     withCredentials([giteaPersonalAccessToken(
-        credentialsId: "personalAccessTokenId",
-        variable: "API_TOKEN2"
+        credentialsId: 'personalAccessTokenId',
+        variable: 'API_TOKEN2'
     )]) {
         println "Token2 is ${API_TOKEN2.substring(1)}"
     }
