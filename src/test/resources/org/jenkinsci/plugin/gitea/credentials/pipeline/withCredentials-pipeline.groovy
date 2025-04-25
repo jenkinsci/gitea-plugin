47258b93cc02d1e2bcaa5650157e118e7b84a170
Mark Waite <mark.earl.waite@gmail.com>
2025-04-25 05:14:51 -0600

    Use echo instead of println
    The echo step is more common in Pipeline

--- src/test/resources/org/jenkinsci/plugin/gitea/credentials/pipeline/withCredentials-pipeline.groovy
+++ src/test/resources/org/jenkinsci/plugin/gitea/credentials/pipeline/withCredentials-pipeline.groovy
@@ -4,12 +4,12 @@ node {
         credentialsId: 'personalAccessTokenId',
         variable: 'API_TOKEN1'
     ]]) {
-        println "Token1 is ${API_TOKEN1.substring(1)}"
+        echo "Token1 is ${API_TOKEN1.substring(1)}"
     }
     withCredentials([giteaPersonalAccessToken(
         credentialsId: 'personalAccessTokenId',
         variable: 'API_TOKEN2'
     )]) {
-        println "Token2 is ${API_TOKEN2.substring(1)}"
+        echo "Token2 is ${API_TOKEN2.substring(1)}"
     }
 }
